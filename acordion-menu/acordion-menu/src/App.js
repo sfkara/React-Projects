384e385c07d8bfc4990c51cad668b778b31fcbf3
sfkara <firatkaramail@gmail.com>
2022-07-14 09:22:20 +0300

    acordion init

---
+++ acordion-menu/acordion-menu/src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import Question from "./components/Question";
+
+
+const App = ()=>{
+    return(
+        <div>
+            <h1>akordiyon moruk</h1>
+            <Question></Question>
+           
+        </div>
+    )
+}
+
+export default App
