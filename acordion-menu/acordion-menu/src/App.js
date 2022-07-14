7a0ddbd6f992f9fd73955f5d901ca7410fc35894
sfkara <firatkaramail@gmail.com>
2022-07-14 10:38:38 +0300

    finalize acordion app

--- acordion-menu/acordion-menu/src/App.js
+++ acordion-menu/acordion-menu/src/App.js
@@ -1,14 +1,22 @@
 import React, {useState} from "react";
 import Question from "./components/Question";
+import data from './components/Data'
 
 
 const App = ()=>{
+    const [question,setQuestion] = useState(data)
+   
     return(
-        <div>
-            <h1>akordiyon moruk</h1>
-            <Question></Question>
-           
-        </div>
+       <main>
+            <div className="container">
+                <h3>Questions and Answers</h3>
+                <section className="info">
+                    {question.map((firat)=>{
+                        return <Question key={firat.id} {...firat}/>
+                    })}
+                </section>
+            </div>
+       </main>
     )
 }
 
--- acordion-menu/acordion-menu/src/components/Question.jsx
+++ acordion-menu/acordion-menu/src/components/Question.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Question = () => {
-  return <h2>question component</h2>;
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const Question = ({ info, title }) => {
+  const [goster, setGoster] = useState(false);
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setGoster(!goster)}>
+          {goster ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {/* {goster ? <p>{info}</p> : ""} */}
+      {goster && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
