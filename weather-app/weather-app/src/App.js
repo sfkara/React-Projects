058be27e9663dff4f04526e078f2ac1a66c0fb25
sfkara <firatkaramail@gmail.com>
2022-07-17 18:18:19 +0300

    weather app init

---
+++ weather-app/weather-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1 className="h1"> this weather app setup</h1>
+    </div>
+  )
+}
+
+export default App
