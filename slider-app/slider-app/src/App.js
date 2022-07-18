b877879e3ea7c122f23880d6600c90a36e472a9b
sfkara <firatkaramail@gmail.com>
2022-07-19 00:27:32 +0300

    slider init

---
+++ slider-app/slider-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import data from './components/Data'
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1> hello from slider</h1>
+    </div>
+  )
+}
+export default App
