41e82e82ac37fd9bc90c74f1be465abd9f158721
sfkara <firatkaramail@gmail.com>
2022-09-01 20:50:37 +0300

    complete lorem ipsum generator

--- lorem-ipsum-generator/lorem-ipsum-generator/src/App.js
+++ lorem-ipsum-generator/lorem-ipsum-generator/src/App.js
@@ -2,8 +2,46 @@ import React, { useState } from 'react'
 import data from './data'
 const App = () => {
   const [text, setText] = useState([])
+  const [count, setCount] = useState(0)
 
-  return <div>App</div>
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('hello lorem')
+    let amount = parseInt(count)
+    if (count <= 0) {
+      amount = 1
+    }
+    if (count > 8) {
+      amount = 8
+    }
+    setText(data.slice(0, amount))
+  }
+  return (
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {/* <p>Lorem ipsum dolior sit amet</p>
+        <p>Lorem ipsum dolior sit amet</p> */}
+
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
+    </section>
+  )
 }
 
 export default App
