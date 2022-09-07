9ca44c25a92bea2d2c10bde5ff54036d716deed1
sfkara <firatkaramail@gmail.com>
2022-09-07 13:46:08 +0300

    structure color

--- color-generator/color-generator/src/App.js
+++ color-generator/color-generator/src/App.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+import Values from 'values.js'
 
 const App = () => {
-  return <div>Color Generator Init</div>
-}
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [lst, setLst] = useState([])
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('f u science')
+  }
+
+  return (
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => {
+              setColor(e.target.value)
+            }}
+            placeholder="#333555777"
+          />
+          <button type="submit" className="btn">
+            Generate
+          </button>
+        </form>
+      </section>
+
+      <section cla>
+        <h3> list goes here</h3>
+      </section>
+    </>
+  )
+}
 export default App
